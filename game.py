ae7f9658dd49e851981853c54b530d81ecd0b5cb
Ayush Pareek <126587776+Ayush1-Explorer@users.noreply.github.com>
2024-07-08 14:32:44 +0530

    Create game.py

---
+++ game.py
@@ -0,0 +1,22 @@
+import random
+
+def game():
+    print("you are playing a game...")
+    score = random.randint(0, 99)
+    with open("hiscore.txt") as f:
+        hiscore = f.read()
+        if (hiscore != ""):
+            hiscore = int(hiscore)
+        else:
+            hiscore = 0
+            
+    print(f"your score: {score}")
+    if(score > hiscore):
+        with open("hiscore.txt", "w") as f:
+            f.write(str(score))
+            
+    return score
+
+game()
+            
+            
